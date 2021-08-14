1827b08cddfda5a50bf8a368699d7dd762051209
Tejvir S. Mann <51454024+tejvirmann@users.noreply.github.com>
2021-08-14 12:05:58 -0500

    Initial Commit

---
+++ Gallify/GallifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GallifyApp.swift
+//  Gallify
+//
+//  Created by Tejvir Mann on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GallifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
