f3a9c830f1f5deeaa618f0b3a47b0f5ccf64bc0b
Shruti <sharma224@gmail.com>
2022-03-21 16:57:12 -0500

    testing

--- Gallify/ViewControllers/Core/Collections/Reel/Helpers/CollectionReelHeader.swift
+++ Gallify/ViewControllers/Core/Collections/Reel/Helpers/CollectionReelHeader.swift
@@ -15,9 +15,9 @@ struct CollectionReelHeader: View {
     @EnvironmentObject var firestoreQuery : FirestoreQuery
     @Environment(\.presentationMode) var presentationMode
     
-    @StateObject var modelsViewModel = ModelsViewModel()
-    
-    @StateObject var placementSettings = PlacementSettings()
+//    @StateObject var modelsViewModel = ModelsViewModel()
+//
+//    @StateObject var placementSettings = PlacementSettings()
     
     let screenWidth: CGFloat
     let screenHeight: CGFloat
@@ -85,21 +85,21 @@ struct CollectionReelHeader: View {
            
             Spacer() 
             
-            if (ARConfiguration.isSupported) {
-                 NavigationLink(destination: FullARView()
-                                 .environmentObject(modelsViewModel)
-                                 .environmentObject(placementSettings)
-                                 .environmentObject(firestoreQuery)
-                               // .onAppear{ async{ await firestoreQuery.fetchModelData()}} //called in reels.
-                     
-                 ,
-                    label: {
-                     Image(systemName: "arkit")
-                         .font(.system(size: 30))
-                         .padding(.leading)
-                         .foregroundColor(.primary)
-                 })
-             }
+//            if (ARConfiguration.isSupported) {
+//                 NavigationLink(destination: FullARView()
+//                                 .environmentObject(modelsViewModel)
+//                                 .environmentObject(placementSettings)
+//                                 .environmentObject(firestoreQuery)
+//                               // .onAppear{ async{ await firestoreQuery.fetchModelData()}} //called in reels.
+//                     
+//                 ,
+//                    label: {
+//                     Image(systemName: "arkit")
+//                         .font(.system(size: 30))
+//                         .padding(.leading)
+//                         .foregroundColor(.primary)
+//                 })
+//             }
             
             
             
