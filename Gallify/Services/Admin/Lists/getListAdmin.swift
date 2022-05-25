786837f65a02ce1400a7f118691b04f4f78090bc
Patron <Patron@loan-lib-a0033.lan>
2022-05-25 14:26:45 -0500

    searchType correction

--- Gallify/Services/Admin/Lists/getListAdmin.swift
+++ Gallify/Services/Admin/Lists/getListAdmin.swift
@@ -12,15 +12,15 @@ extension FirestoreQuery {
     /**
         Gets all art that have not been approved yet
         Stores it in an array form to be displayed in approved screen
+        Unapproved = 2, approved = 1
      */
     func getArtInReview() async {
         
-        print("GET NEXT IS SET TO = ", self.getNext)
         if(self.getNext){ //makes query get next batch of art for review
             self.inReviewQuery = try? await self.inReviewQuery.start(afterDocument: self.lastDoc)
         }
         else{ //This is the first batch to be fetched
-            self.inReviewQuery = try await FirestoreQuery.db.collection("art").whereField("searchType", isEqualTo: 1)
+            self.inReviewQuery = try await FirestoreQuery.db.collection("art").whereField("searchType", isEqualTo: 2)
                               .order(by: "popularity", descending: true)
                               .limit(to: 11)
         }
@@ -33,19 +33,23 @@ extension FirestoreQuery {
                     print("Error getting documents: \(err)")
                 } else {
                     var i = 0
-                    for document in querySnapshot!.documents {
-                       
-                       inReviewArtArr = querySnapshot!.documents.compactMap { querySnapshot -> Art? in
-                                return try? querySnapshot.data(as: Art.self)
-                       }
-                        print(inReviewArtArr[i].name, " " , inReviewArtArr[i].artId)
-                        i += 1
-                       self.lastDoc = querySnapshot!.documents.last //get last document for later.
-                       self.getNext = true
+                    inReviewArtArr = querySnapshot!.documents.compactMap { querySnapshot -> Art? in
+                             return try? querySnapshot.data(as: Art.self)
                     }
+                    //print("is inReviewArtArr empty ? ", inReviewArtArr.isEmpty)
+//                    for art in inReviewArtArr {
+//                        print(art.name, " " , art.artId)
+//                        i += 1
+//                    }
+                    self.lastDoc = querySnapshot!.documents.last //get last document for later.
+                    self.getNext = true
                     self.artInReview = inReviewArtArr
                 }
         }
+        
+    }
+    
+    func approveArt() async{
         
     }
 }
