47a7f081aad2abc9781655e3a2dd52f96d2dd81a
Tejvir S. Mann <tejvirmann11@gmail.com>
2022-05-29 09:57:28 -0500

    updated art model, user and playlist otw

--- Gallify/Models/Art/Art.swift
+++ Gallify/Models/Art/Art.swift
@@ -37,6 +37,7 @@ class Art: Encodable, Decodable, ObservableObject, Identifiable {
         case genre
         case history
         //case latestHistoryDate
+        //case latestPurchaseDate
         case likes
         case location
         case metadataUrl
@@ -51,25 +52,18 @@ class Art: Encodable, Decodable, ObservableObject, Identifiable {
         case storageName
         case thumbnailUrl
         case tokenId
-        
+        case unlockableContent
+        case geoContent
+        case claimableIfNearOnly
+        case ownableIfNearOnly
+        case licenseType
+        case latestPurchasePrice
+        case latestPrice
+        case isListing
+        case collectionRef
+        case creatorRef
+        case ownerRef
 
-        //case unlockable content
-        //case geographic content
-        //case forfree if near
-        //case forsale if near
-        //case report
-        //what type of licensing does the owner have?
-        //"collectionRef": "collections/id",
-        //  "creatorRef": "users/id",
-       // "ownerRef": "users/id",
-       // "likesCount": 0,
-       // "createdDate": "Timestamp",
-       // "latestHistoryDate": "Timestamp",
-       // "latestPrice": "",
-       // "latestPurchaseDate": "Timestamp",
-       // "latestPurchasePrice": "",
-       // "isListing": false
-        
     }
     
     
@@ -103,8 +97,22 @@ class Art: Encodable, Decodable, ObservableObject, Identifiable {
     @Published var thumbnailUrl: String
     @Published var tokenId: Int
     
+    //new params as of 05/20/22
+    @Published var unlockableContent: String
+    @Published var geoContent: String
+    @Published var claimableIfNearOnly: Bool
+    @Published var ownableIfNearOnly: Bool
+    @Published var licenseType: String
+    @Published var latestPurchasePrice: String
+    @Published var latestPrice: String
+    //@Published var latestPurchaseDate: Timestamp
+    @Published var isListing: Bool
+    @Published var collectionRef: String
+    @Published var creatorRef: String
+    @Published var ownerRef: String
 
     
+
     init() {
     
         artId = ""
@@ -135,6 +143,20 @@ class Art: Encodable, Decodable, ObservableObject, Identifiable {
         storageName = ""
         thumbnailUrl = ""
         tokenId = 0
+        
+        // latestPurchaseDate = Timestamp()
+        unlockableContent = ""
+        geoContent = ""
+        claimableIfNearOnly = false
+        ownableIfNearOnly = false
+        licenseType = ""
+        latestPurchasePrice = ""
+        latestPrice = ""
+        isListing = true
+        collectionRef = ""
+        creatorRef = ""
+        ownerRef = ""
+        
        
 
     }
@@ -175,6 +197,20 @@ class Art: Encodable, Decodable, ObservableObject, Identifiable {
         thumbnailUrl = try container.decode(String.self, forKey: .thumbnailUrl)
         tokenId = try container.decode(Int.self, forKey: .tokenId)
         
+        //latestPurchaseDate = try container.decode(Timestamp.self, forKey: .latestPurchaseDate)
+        unlockableContent = try container.decode(String.self, forKey: .unlockableContent)
+        geoContent = try container.decode(String.self, forKey: .geoContent)
+        claimableIfNearOnly = try container.decode(Bool.self, forKey: .claimableIfNearOnly)
+        ownableIfNearOnly = try container.decode(Bool.self, forKey: .ownableIfNearOnly)
+        licenseType = try container.decode(String.self, forKey: .licenseType)
+        latestPurchasePrice = try container.decode(String.self, forKey: .latestPurchasePrice)
+        latestPrice = try container.decode(String.self, forKey: .latestPrice)
+        isListing = try container.decode(Bool.self, forKey: .unlockableContent)
+        collectionRef = try container.decode(String.self, forKey: .collectionRef)
+        creatorRef = try container.decode(String.self, forKey: .creatorRef)
+        ownerRef = try container.decode(String.self, forKey: .ownerRef)
+
+        
     }
     
     
@@ -212,8 +248,19 @@ class Art: Encodable, Decodable, ObservableObject, Identifiable {
         try container.encode(thumbnailUrl, forKey: .thumbnailUrl)
         try container.encode(tokenId, forKey: .tokenId)
         
-        
-        
+        //try container.encode(latestPurchaseDate, forKey: .latestPurchaseDate)
+        try container.encode(unlockableContent, forKey: .unlockableContent)
+        try container.encode(geoContent, forKey: .geoContent)
+        try container.encode(claimableIfNearOnly, forKey: .claimableIfNearOnly)
+        try container.encode(ownableIfNearOnly, forKey: .ownableIfNearOnly)
+        try container.encode(licenseType, forKey: .licenseType)
+        try container.encode(latestPrice, forKey: .latestPrice)
+        try container.encode(latestPurchasePrice, forKey: .latestPurchasePrice)
+        try container.encode(isListing, forKey: .isListing)
+        try container.encode(collectionRef, forKey: .collectionRef)
+        try container.encode(creatorRef, forKey: .creatorRef)
+        try container.encode(ownerRef, forKey: .ownerRef)
+
         
     }
     
