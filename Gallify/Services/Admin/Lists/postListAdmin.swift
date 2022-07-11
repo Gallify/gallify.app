3648d1d83cdf512fc2f081b275bf4796fe0f6ba3
Patron <Patron@loan-lib-a0033.lan>
2022-07-11 01:59:00 -0500

    postToMarketplace method working now

--- Gallify/Services/Admin/Lists/postListAdmin.swift
+++ Gallify/Services/Admin/Lists/postListAdmin.swift
@@ -17,32 +17,9 @@ extension FirestoreQuery {
 func postArtToMarketplace(collectionId : String, artId:String) async  {
     let apiAddress = "https://api.gallify.app/v0/mint/\(collectionId)/\(artId)"
     let url = URL(string: apiAddress)!
-//    let body = "\(collectionId)/\(artId)"
-//
-//    let finalBody = body.data(using: .utf8)
+
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
-//    request.httpBody = finalBody
-
-    
-   // var request = URLRequest(url: url)
-//    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-    //request.httpMethod = "POST"
-    
-//    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-//        if let error = error {
-//            print( "DataTask error: " + error.localizedDescription + "\n")
-//        } else if
-//            let data = data,
-//            let response = response as? HTTPURLResponse,
-//            response.statusCode == 200 {
-//                DispatchQueue.main.async {
-//                    print("Successfully posted art to marketplace");
-//                }
-//            }
-//    }
-//    task.resume()
-    
 
     URLSession.shared.dataTask(with: request){
                (data, response, error) in
